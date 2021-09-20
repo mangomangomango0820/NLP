e3d898d5f8dc69d2fdb0f5d73aec8ac5db417857
MissMango0820 <zhangxueshan0820@hotmail.com>
2021-09-21 00:52:53 +0800

    20210921 remove notes

--- wordcloud.py
+++ wordcloud.py
@@ -9,27 +9,25 @@ import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 from imageio import imread
 
-# 读取文件
+# 1. read file
 f = open('C://Users//Administrator//Desktop//practice.txt','rt')
 fd = f.read()
-# 等同于 f = open('C://Users//Administrator//Desktop//practice.txt', 'rt').read()
+# f = open('C://Users//Administrator//Desktop//practice.txt', 'rt').read()
 f.close()
 
-# 文本预处理
+# 2. preprocess file content
 pattern = re.compile(u'\t|\'|\n|\.|-|_|#|：|:|；|\||/||(|)|\?|"')
 fd = re.sub(pattern, '', fd)
 
-# 文本分词
 seg = jieba.cut(fd, cut_all=False)
 # print(type(seg))
 # print(seg)
 
-# 去除词库
 # rwlist = [line.strip() for line in open('C://Users//Administrator//Desktop//rw.txt').readlines()]
 # rwlist = [rw.strip() for rw in open('C://Users//Administrator//Desktop//rw.txt').read()]
 rwlist = [u'-', ' ', 'u', '@', ',', u'( ', u')']
 
-# 文本清洗
+# 3. process file content
 words = []
 for word in seg:
     if word not in rwlist and len(word) >= 1:
@@ -38,15 +36,15 @@ for word in seg:
 cloud_text = ",".join(words)
 # cloud_text = words
 
-# 词频统计
+# 4. collect word counts
 count = collections.Counter(cloud_text)
 count_top5 = count.most_common(5)
 print(type(count_top5))
 for i in count_top5:
     print('character: ', i[0], ';', 'count: ', i[1])
 
-
-jpg = imread('C://Users//Administrator//Desktop//2.png')
+# 5. image process & generaate wordcloud
+jpg = imread('C://Users//Administrator//Desktop//image.png')
 
 wc = WordCloud(
     mask = jpg,
